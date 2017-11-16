b4f026724c9b85b726e07b5cd42dae6a61543ab8
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-11-16 05:12:38 -0500

    Initial Commit

---
+++ Assets/Scripts/GameSetup.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSetup : MonoBehaviour
+{
+	static int gameScore = 0;
+
+    static GameObject ball;
+    public GameObject ballRef;
+
+	// Use this for initialization
+	void Start () 
+	{
+        dropBall();
+	}
+
+    void dropBall()
+    {
+        ball = Instantiate (ballRef) as GameObject;
+        float ballXPosition = Random.Range (-570, 570) / 100.0f;
+        ball.transform.position = new Vector3 (ballXPosition, 5, 0);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+    }
+}
