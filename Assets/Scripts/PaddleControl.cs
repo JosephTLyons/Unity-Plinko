de029c3ede12dcd5426073a7a51fe049b87c32f9
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-11-16 16:39:19 -0500

    Paddle moves with keys now

--- Assets/Scripts/PaddleControl.cs
+++ Assets/Scripts/PaddleControl.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PaddleControl : MonoBehaviour 
 {
+    public KeyCode moveLeft, moveRight;
+
+    public float speedX = 0;
     
 	void Start()
     {
@@ -11,6 +14,26 @@ public class PaddleControl : MonoBehaviour
 
 	void Update() 
     {
+        // move the player in the 4 directions based on the keys we set up for it
+        if (Input.GetKey (moveLeft)) 
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-speedX, 0f);
+            //rigidbody2D.AddForce(new Vector2( 0, -rigidbody2D.velocity.y));
+            //rigidbody2D.AddForce(new Vector2(0, speed));
+        } 
+
+        else if (Input.GetKey (moveRight)) 
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speedX, 0f);
+            //rigidbody2D.AddForce(new Vector2(0, -rigidbody2D.velocity.y));
+            //rigidbody2D.AddForce(new Vector2(0, -speed));
+        }
+
+        else 
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+            //rigidbody2D.AddForce(new Vector2(0, -rigidbody2D.velocity.y));
+        }
 	}
 
     void OnCollisionEnter (Collision col)
