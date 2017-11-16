37f73e8782914161ec1f28ab7f16f2c1d594c0e6
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-11-16 16:25:47 -0500

    Added scripts and changed tags

---
+++ Assets/Scripts/PaddleControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleControl : MonoBehaviour 
+{
+    
+	void Start()
+    {
+    }
+
+	void Update() 
+    {
+	}
+
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
