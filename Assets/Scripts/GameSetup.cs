01597f03693db15c43703ecbd5f2dd76f0c994ca
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-11-16 17:58:42 -0500

    Working on system to increase ball drop

--- Assets/Scripts/GameSetup.cs
+++ Assets/Scripts/GameSetup.cs
@@ -4,25 +4,28 @@ using System.Collections;
 public class GameSetup : MonoBehaviour
 {
 	static int gameScore = 0;
+    int ballCount;
 
     static GameObject ball;
     public GameObject ballRef;
 
-	// Use this for initialization
 	void Start () 
 	{
-        dropBall();
+        ballCount = 1;
 	}
+
+	void Update()
+    {
+        if ((ballCount--) > 0)
+        {
+            dropBall();
+        }
+    }
 
     void dropBall()
     {
         ball = Instantiate (ballRef) as GameObject;
         float ballXPosition = Random.Range (-570, 570) / 100.0f;
         ball.transform.position = new Vector3 (ballXPosition, 6, 0);
-    }
-	
-	// Update is called once per frame
-	void Update ()
-    {
     }
 }
