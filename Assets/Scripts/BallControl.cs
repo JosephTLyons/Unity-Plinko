9cc50241859fd04b43a7938616ee7df9631b0e93
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-11-16 16:51:12 -0500

    Collisions now work

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour 
+{
+	void Update () 
+    {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // Play swallow ball sound
+            Destroy (gameObject);
+        }
+
+        else if (collision.gameObject.tag == "Peg")
+        {
+            // Play peg sound
+        }
+    }
+}
