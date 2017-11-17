8c82befd60e6fd55816101b036c0bc9aedb64f01
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-11-16 19:58:59 -0500

    reduced height to prevent bounce on game start

--- Assets/Scripts/GameSetup.cs
+++ Assets/Scripts/GameSetup.cs
@@ -34,6 +34,6 @@ public class GameSetup : MonoBehaviour
     {
         ball = Instantiate (ballRef) as GameObject;
         float ballXPosition = Random.Range (-570, 570) / 100.0f;
-        ball.transform.position = new Vector3 (ballXPosition, 6, 0);
+        ball.transform.position = new Vector3 (ballXPosition, 5.5f, 0);
     }
 }
