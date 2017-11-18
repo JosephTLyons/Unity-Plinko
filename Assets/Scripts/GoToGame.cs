55169a5f9a9037085176f3886311a9d0bd584725
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-11-18 02:48:46 -0500

    Added an opening scene with rules

---
+++ Assets/Scripts/GoToGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToGame : MonoBehaviour 
+{
+	void Update() 
+    {
+        if (Input.GetKeyUp (KeyCode.Space))
+        {
+            SceneManager.LoadScene ("Game");
+        }
+	}
+}
