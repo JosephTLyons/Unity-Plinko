fdee14f8a6e300bba73c9e8330be53968a75b6dd
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-11-20 17:59:25 -0500

    Comment

--- Assets/Scripts/HighScore.cs
+++ Assets/Scripts/HighScore.cs
@@ -9,6 +9,8 @@ public class HighScore : MonoBehaviour
 
 	void Start() 
     {
+        // High score is split into two objects, the object of the label and the object that has the script
+        // This is so I can use DontDestroyOnLoad() to keep the script from being destroyed and score from being lost
         DontDestroyOnLoad (this);
         highScoreLabelRef.GetComponent<TextMesh>().text = "High Score: " + highScore;
 	}
